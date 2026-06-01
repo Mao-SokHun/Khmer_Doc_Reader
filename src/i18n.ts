b77eb952fd2155b1d837b65ff0cf58601149d36f
Mao-SokHun <sokhunmao390@gmail.com>
2026-06-01 20:02:49 +0700

    test: change document tabs label

--- src/i18n.ts
+++ src/i18n.ts
@@ -2,7 +2,7 @@ export type Language = 'kh' | 'en';
 
 export const translations = {
   kh: {
-    documentTabs: 'Testing change',
+    documentTabs: 'Testing ',
     createTab: 'បង្កើតផ្ទាំងថ្មី',
     addLesson: 'បន្ថែមមេរៀនថ្មី...',
     edit: 'កែសម្រួល',
